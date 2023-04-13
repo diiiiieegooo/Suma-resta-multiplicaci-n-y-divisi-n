4bef9b5f082146209d4e0995cf37178acdb2caa1
diiiiieegooo <123724300+diiiiieegooo@users.noreply.github.com>
2023-04-13 11:08:17 +0200

    cambios

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let num2 = parseFloat(prompt("Ahora, ponga el segundo número aquí: "));
 let suma = num1 + num2;
 let resta = num1 - num2;
 let multiplicacion = num1 * num2;
-let division = num2 !== 0 ? num1 / num2 : "No se puede dividir por cero.";
+let division = num2 !== 0 ? num1 / num2 : "No es posible realizar operaciones por cero.";
 
 alert("El resultado de la suma es: " + suma);
 alert("El resultado de la resta es: " + resta);
