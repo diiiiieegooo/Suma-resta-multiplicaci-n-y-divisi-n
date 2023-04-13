a13bf099683dee34680c30a0d56328f29bc07df3
diiiiieegooo <123724300+diiiiieegooo@users.noreply.github.com>
2023-04-13 10:57:16 +0200

    cambios

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-console.log("¡Bienvenido! Este programa realizará operaciones matemáticas básicas.");
+alert("¡Bienvenido! Este programa realizará operaciones matemáticas básicas.");
 
-console.log("Por favor, ingrese el primer número:");
-let num1 = parseFloat(prompt("Número 1: "));
+let num1 = parseFloat(prompt("Por favor, ingrese el primer número: "));
 
-console.log("Ahora, ingrese el segundo número:");
-let num2 = parseFloat(prompt("Número 2: "));
+let num2 = parseFloat(prompt("Ahora, ingrese el segundo número: "));
 
-console.log("Seleccione la operación que desea realizar")
 let suma = num1 + num2;
 let resta = num1 - num2;
 let multiplicacion = num1 * num2;
 let division = num2 !== 0 ? num1 / num2 : "No se puede dividir por cero.";
 
-console.log("El resultado de la suma es:", suma);
-console.log("El resultado de la resta es:", resta);
-console.log("El resultado de la multiplicación es:", multiplicacion);
-console.log("El resultado de la división es:", division);
+alert("El resultado de la suma es: " + suma);
+alert("El resultado de la resta es: " + resta);
+alert("El resultado de la multiplicación es: " + multiplicacion);
+alert("El resultado de la división es: " + division);
